d91bf269ef3f0d1c5883ffae4b1d2897dc818b0e
Owen Lehmer <olehmer@gmail.com>
2016-07-21 17:20:46 -0700

    clustering placeholder

---
+++ Clustering/kmeans.jl
@@ -0,0 +1,10 @@
+#=
+Owen Lehmer - 7/20/16
+
+This module will run the k-means algorithm to cluster data. 
+
+So this is already done in the Clustering package! Very nice. Just use that!
+
+Website: http://clusteringjl.readthedocs.io/en/latest/#
+=#
+
